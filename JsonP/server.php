57b7367e235aa4ad74f0541fa1ef1997ad5d42d9
lieyan <2377544615@qq.com>
2017-05-23 00:47:42 -0700

    JsonP Code backup

---
+++ JsonP/server.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/23
+ * Time: 10:54
+ */
+$data = [
+    [
+        'username'=>'dumin',
+        'age'=>22
+    ],
+    [
+        'username'=>'lieyan',
+        'age'=>25
+    ]
+];
+echo  json_encode($data);
